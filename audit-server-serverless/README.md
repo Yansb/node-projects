dd76f2821ff2063bc2da19f2140f89850802fff3
Yansb <yansbarreiro@gmail.com>
2022-04-14 23:34:01 +0000

    feat: lambda serverless function to retunr deleted accounts

---
+++ audit-server-serverless/README.md
@@ -0,0 +1,5 @@
+## Montando uma base de auditoria com Serverless, SNS, SQS, Lambda e DynamoDB
+
+aula do |[André Spanguero Kanayama](https://github.com/askmon)|
+
+na rocketseat
---
+++ audit-server-serverless/src/consumer.js
@@ -0,0 +1,23 @@
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
+const getUuid = require('uuid-by-string')
+
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
+const recordDeletedAccountInfo = event => {
+  const {email, timestamp} = JSON.parse(event.Records[0].body)
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      userId: getUuid(email, 'yan'),
+      deletedAt: timestamp,
+    },
+  }
+
+  return documentClient.put(params).promise()
+}
+
+module.exports = {
+  recordDeletedAccountInfo,
+}
